5378666673a7e8c29e1d1879ef2641983127f313
Oxana Odegova <oxyapple@yandex.ru>
2020-12-10 21:04:33 +0100

    first set up

--- src/index.js
+++ src/index.js
@@ -1,17 +1,35 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
 
+const App = () => {
+  return (
+    <div className="ui container comments">
+      <div className="comment">
+        <a href="/" className="avatar">
+          <img alt="avatar" src='https://source.unsplash.com/random'/>
+        </a>
+        <div className="contant">
+          <a href="/" className="author">
+            Sam
+        </a>
+          <div className="metadata">
+            <span className="date">
+              Today at 6 pm
+        </span>
+          </div>
+          <div className="text">
+            Nice blog post!
+        </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+// Take the react component and show it on the screen
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+  <App />,
+  document.querySelector('#root')
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+
